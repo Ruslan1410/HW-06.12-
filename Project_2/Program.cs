f82dcd7c2d09bf1d75ef92606e3a8fcd6385d48c
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-06 17:00:17 +0300

    project 2

---
+++ Project_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Пример строки с латинскими буквами в обоих регистрах
+        string originalString = "HeLLo WoRLd";
+
+        Console.WriteLine("First string: " + originalString);
+
+        // Заменяем все заглавные буквы на строчные
+        string resultString = ConvertToLowerCase(originalString);
+
+        Console.WriteLine("Second string: " + resultString);
+    }
+
+    // Метод для замены заглавных букв на строчные
+    static string ConvertToLowerCase(string input)
+    {
+        char[] charArray = input.ToCharArray();
+
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            if (char.IsUpper(charArray[i]))
+            {
+                charArray[i] = char.ToLower(charArray[i]);
+            }
+        }
+
+        return new string(charArray);
+    }
+}
